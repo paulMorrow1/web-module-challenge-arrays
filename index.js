12ad9657123160800a045cfb19aa67df91a415b3
Paul Morrow <pmorow93@gmail.com>
2022-02-09 23:56:24 -0600

    Finished assignment

--- index.js
+++ index.js
@@ -170,9 +170,15 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
-}
+function filterByWord(originalFlavors, flavor){
+  const filteredArray = [];
+  for (let i = 0; i < originalFlavors.length; i++){
+  if (originalFlavors[i].includes(flavor)){
+    filteredArray.push(originalFlavors[i])
+  }
+     
+    } return filteredArray;
+};
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
